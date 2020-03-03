06bc4031ee27b1074333d67f0389d79bfb57d6ed
JEGNUI <58769364+JEGNUI@users.noreply.github.com>
2020-03-03 16:59:49 +0800

    Update wb_launch.js

--- weibo/wb_launch.js
+++ weibo/wb_launch.js
@@ -1,5 +1,11 @@
 /*
-README：https://github.com/yichahucha/surge/tree/master
+Weibo remove start up ads
+
+[rewrite_local]
+^https?://m?api\.weibo\.c(n|om)/2/(statuses/(unread|extend|positives/get|(friends|video)(/|_)timeline)|stories/(video_stream|home_list)|(groups|fangle)/timeline|profile/statuses|comments/build_comments|photo/recommend_list|service/picfeed|searchall|cardlist|page|\!/photos/pic_recommend_status) url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/wb_ad.js
+^https?://(sdk|wb)app\.uve\.weibo\.com(/interface/sdk/sdkad.php|/wbapplua/wbpullad.lua) url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/wb_launch.js
+[mitm]
+hostname = api.weibo.cn, mapi.weibo.com, *.uve.weibo.com
  */
 
 const path1 = "/interface/sdk/sdkad.php";
@@ -25,3 +31,5 @@ if (url.indexOf(path2) != -1) {
 }
 
 $done({body});
+
+// by yichahucha
