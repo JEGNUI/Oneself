d11c18c03b924ac8b319c774976d9575b92c64f3
JEGNUI <58769364+JEGNUI@users.noreply.github.com>
2020-03-03 17:11:21 +0800

    Update yyzs.js

--- yyzs.js
+++ yyzs.js
@@ -2,17 +2,12 @@
 By Primovist
 
 Surge:
-
-[Script]
 http-response ^https?:\/\/(i|newdrugs)\.dxy\.cn\/(snsapi\/username\/|app\/user\/(pro\/stat\?|init\?timestamp=)) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/yyzs.js
 
-Qx
-
+Qx:
 ^https?:\/\/(i|newdrugs)\.dxy\.cn\/(snsapi\/username\/|app\/user\/(pro\/stat\?|init\?timestamp=)) url script-response-body Surge/JS/yyzs.js
 
-[MITM]
-
-hostname = newdrugs.dxy.cn
+[MITM]hostname = newdrugs.dxy.cn
 */
 
 const path1 = "/snsapi/username/";
