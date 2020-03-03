af03c493fa0ea7a886a920f4532b58c9e840dfe8
JEGNUI <58769364+JEGNUI@users.noreply.github.com>
2020-03-03 20:42:44 +0800

    Update Aweme.js

--- Aweme.js
+++ Aweme.js
@@ -1,7 +1,6 @@
 /*
 [Script]
 http-request ^https:\/\/.*\.amemv\.com\/aweme\/v.*\/(feed|post) script-path=https://Choler.github.io/Surge/Script/Aweme.js
-http-response ^https:\/\/.*\.amemv\.com\/aweme\/v.*\/(feed|post) requires-body=true,script-path=https://Choler.github.io/Surge/Script/Aweme.js
 
 [MITM]
 hostname = *.amemv.com
