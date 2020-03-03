34d807c1baad25c734bab89ed102c5faf8cf81b3
JEGNUI <58769364+JEGNUI@users.noreply.github.com>
2020-03-03 17:38:09 +0800

    用药助手

--- yyzs.js
+++ yyzs.js
@@ -1,9 +1,6 @@
 /*
 By Primovist
 
-Surge:
-http-response ^https?:\/\/(i|newdrugs)\.dxy\.cn\/(snsapi\/username\/|app\/user\/(pro\/stat\?|init\?timestamp=)) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/yyzs.js
-
 Qx:
 ^https?:\/\/(i|newdrugs)\.dxy\.cn\/(snsapi\/username\/|app\/user\/(pro\/stat\?|init\?timestamp=)) url script-response-body Surge/JS/yyzs.js
 
