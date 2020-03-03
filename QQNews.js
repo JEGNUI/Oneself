0ebd09658e58265c34b004a5d4abe7c176ebe437
JEGNUI <58769364+JEGNUI@users.noreply.github.com>
2020-03-03 20:41:57 +0800

    Update QQNews.js

--- QQNews.js
+++ QQNews.js
@@ -2,6 +2,7 @@
 [Script]
 http-response https://r\.inews\.qq.com\/get(QQNewsUnreadList|RecommendList) requires-body=1,max-size=-1,script-path=https://Choler.github.io/Surge/Script/QQNews.js
 
+
 [MITM]
 hostname = r.inews.qq.com
 */
