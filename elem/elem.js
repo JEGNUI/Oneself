a624493482f5bc762faf06b6ef46037dfe1d4605
JEGNUI <58769364+JEGNUI@users.noreply.github.com>
2020-06-22 20:57:08 +0800

    Update elem.js

--- elem/elem.js
+++ elem/elem.js
@@ -1,341 +1,176 @@
 /*
-参考chavyleung和NobyDa的写法
+"饿了么" app(9.0.10) "我的 - 打卡领红包"自动签到，支持 Quantumult X（理论上也支持 Surge、Loon，未尝试）。
+默认已使用 elemSign.js，故请先使用 elemGetCookies.js 获取 Token。(https://github.com/songyangzz/QuantumultX/blob/master/elem/elemGetCookies.js)
+到 cron 设定时间自动签到时，若弹出"饿了么 - 打卡领红包 - 打卡成功"即完成签到，其他提示或无提示请发送日志信息至 issue。
 
-//饿了么
+⚠️免责声明：
+1. 此脚本仅用于学习研究，不保证其合法性、准确性、有效性，请根据情况自行判断，本人对此不承担任何保证责任。
+2. 由于此脚本仅用于学习研究，您必须在下载后 24 小时内将所有内容从您的计算机或手机或任何存储设备中完全删除，若违反规定引起任何事件本人对此均不负责。
+3. 请勿将此脚本用于任何商业或非法目的，若违反规定请自行对此负责。
+4. 此脚本涉及应用与本人无关，本人对因此引起的任何隐私泄漏或其他后果不承担任何责任。
+5. 本人对任何脚本引发的问题概不负责，包括但不限于由脚本错误引起的任何损失和损害。
+6. 如果任何单位或个人认为此脚本可能涉嫌侵犯其权利，应及时通知并提供身份证明，所有权证明，我们将在收到认证文件确认后删除此脚本。
+7. 所有直接或间接使用、查看此脚本的人均应该仔细阅读此声明。本人保留随时更改或补充此声明的权利。一旦您使用或复制了此脚本，即视为您已接受此免责声明。
 
-> 代码已同时兼容 Surge & QuanX, 使用同一份签到脚本即可
+Author：zZPiglet
 
-
-## 配置 (Surge)
-
-```properties
-[MITM]
-h5.ele.me
-
-[Script]
-http-request ^https:\/\/h5\.ele\.me\/restapi\/eus\/v\d\/current_user$ script-path=elemGetCookies.js
-cron "0 5 0 * * *" script-path=elemSign.js
-```
-
-## 配置 (QuanX)
-
-```properties
-[MITM]
-h5.ele.me
-
-[rewrite_local]
-
-^https:\/\/h5\.ele\.me\/restapi\/eus\/v\d\/current_user$ url script-request-header elemGetCookies.js
-
-
-
+Quantumult X (App Store:1.0.5+, TestFlight 190+):
 [task_local]
-5 0 * * * elemSign.js
-```
+1 0 * * * elemCheckIn.js, tag=饿了么-打卡领红包
+or remote
+1 0 * * * https://raw.githubusercontent.com/zZPiglet/Task/master/elem/elemCheckIn.js, tag=饿了么-打卡领红包
 
-## 说明
-
-1. 先把h5.ele.me`加到`[MITM]`
-2. 再配置重写规则:
-   - Surge: 把两条远程脚本放到`[Script]`
-   - QuanX: 把`elemGetCookies.js`和`elemSign.js`传到`On My iPhone - Quantumult X - Scripts` (传到 iCloud 相同目录也可, 注意要打开 quanx 的 iCloud 开关)
-3. 打开 APP, 访问下`我的`>`签到领红包`(左上角)
-4. 系统提示: `获取Cookie: 成功` （如果不提示获取成功, 尝试杀进程再进`个人`>`每日签到`）
-5. 最后就可以把第 1 条脚本注释掉了
-
-> 第 1 条脚本是用来获取 cookie 的, 用浏览器访问一次获取 cookie 成功后就可以删掉或注释掉了, 但请确保在`登录成功`后再获取 cookie.
-
-> 第 2 条脚本是签到脚本, 每天`00:05:00`执行一次.
-
-脚本之前会运行3秒，为了保证两次翻盘成功，建议定时任务配置运行两次
+Surge 4.0+ & Loon:
+[Script]
+cron "1 0 * * *" script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/elem/elemCheckIn.js
 */
 
+const CheckInURL = 'https://h5.ele.me/restapi/acquisition/dailyCheckIn/v1/checkIn'
+const $cmp = compatibility()
+Checkin()
+$cmp.done()
 
-const delay=3000;
-const cookieName = '饿了么'
-const cookieKey = 'cookie_elem'
-const UserId = 'user_id_elem'
-const sy = init()
-var cookieVal = sy.getdata(cookieKey);
-var regx = /USERID=\d+/;
-
-var userid = cookieVal.match(regx)[0];
-userid = userid.replace('USERID=', '');
-
-
-var headerscommon = {
-  'Content-Type': 'application/json',
-  'Cookie': cookieVal,
-  'f-refer': 'wv_h5',
-  'Origin': 'https://tb.ele.me',
-  'Referer': 'https://tb.ele.me/wow/zele/act/qiandao?wh_biz=tm&source=main',
-  'User-Agent': 'Rajax/1 Apple/iPhone11,8 iOS/13.3 Eleme/8.29.6 ID/BFA5A018-7070-4341-9DEF-763E3B23EA282; IsJailbroken/1 Mozilla/5.0 (iPhone; CPU iPhone OS 13_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 AliApp(ELMC/8.29.6) UT4Aplus/0.0.4 WindVane/8.6.0 828x1792 WK'
-}
-
-//签到结果
-var signresult = '';
-
-//翻牌结果
-var turnstr = '翻牌结果: ';
-//翻牌奖励
-var turnresult = new Array;
-
-//签到奖励
-var sign_result = new Array;
-
-var hisresult;
-sign()
-
-function sign() {
-
-  dosignhis().then((data) => {
-    if (hisresult) {
-     if (hisresult.has_signed_in_today) {
-        signresult = `签到结果: 重复❗ 已连续签到${hisresult.current_day+1}天`;
-        turnstr=turnstr+'无';
-        doNotify();
-        sy.done()
-      }
-      else {
-        dosign().then((data) => {
-            doturnover(1,200).then((data) => {
-              doshare().then((data) => {
-
-                doturnover(2,delay).then((data) => {
-      
-                  doNotify();
-                  sy.done()
-                })
-              })
-          })
-        })
-      }
+function Checkin() {
+    let subTitle = ''
+    let detail = ''
+    const daily = {
+        url: CheckInURL,
+        headers: {
+            "Cookie": $cmp.read("cookie_elem")
+        }
     }
-  });
-}
-
-function dosign() {
-
-  return new Promise(resolve => {
-    setTimeout(() => {
-
-      try {
-        var endurl = '/sign_in'
-        url = { url: `https://h5.ele.me/restapi/member/v2/users/`, headers: headerscommon }
-        if (cookieVal == undefined || cookieVal == "0" || cookieVal == null) {
-          sy.msg(cookieName, "未获取Cookie", '');
-          return;
-        }
-
-        url.url += userid;
-        url.url += endurl;
-
-        sy.post(url, (error, response, data) => {
-          var obj = JSON.parse(data);
-          if (response.status == 200) {
-            signresult = `签到结果: 成功🎉 已连续签到${hisresult.current_day+1}天`
-            sign_result = obj;
-
-          } else if (response.status == 400) {
-            signresult = `签到结果: 重复❗ 已连续签到${hisresult.current_day}天`
-
-          }
-          else {
-            signresult = `签到结果: 未知❗ 已连续签到${hisresult.current_day}天`
-          }
-          resolve('done');
-        })
-      }
-      catch (erre) {
-        resolve('done')
-      }
-    })
-  })
-}
-
-function doturnover(count,time) {
-
-  return new Promise(resolve => {
-    setTimeout(() => {
-
-      try {
-        var endurl = '/sign_in/daily/prize'
-        let body = { "channel": "app", "index": random(0, 3), "longitude": 116.334716796875, "latitude": 59.73897171020508 };
-        url = {
-          url: `https://h5.ele.me/restapi/member/v2/users/`,
-          headers: headerscommon,
-          body: JSON.stringify(body)
-        }
-        if (cookieVal == undefined || cookieVal == "0" || cookieVal == null) {
-          sy.msg(cookieName, "未获取Cookie", '');
-          return;
-        }
-        url.url += userid;
-        url.url += endurl;
-        sy.post(url, (error, response, data) => {
-          var obj = JSON.parse(data);
-          sy.log(count);
-          if (response.status == 200) {
-            turnstr = turnstr + `成功(${count})🎉 `
-            for (var i in obj) {
-              turnresult.push(obj[i]);
+    $cmp.post(daily, function(error, response, data) {
+        if (!error) {
+            if (response.statusCode == 401) {
+                subTitle += 'Token 未获取或失效❗'
+                detail += '请按脚本开头注释完成配置并首次或重新获取 Token。'
+            } else {
+                const result = JSON.parse(data)
+                if (result.code == 200) {
+                    subTitle += '打卡成功！🍗'
+                    let todayearn = result.data.checkInAmount / 100
+                    let total = result.data.currentAmount / 100
+                    detail += '打卡获得 ' + todayearn + ' 元，账户共有 ' + total + ' 元红包。'
+                } else if (result.code == 4003) {
+                    subTitle += '重复打卡！🥡'
+                    detail += result.message
+                } else {
+                    subTitle += '打卡失败‼️ 详情请见日志。'
+                    detail += data
+                    $cmp.log("elem failed response : \n" + data)
+                }
             }
-
-          } else if (response.status == 400) {
-            turnstr = turnstr + `重复(${count})❗ `
-
-          }
-          else {
-            turnstr = turnstr + `未知(${count})❗ `
-          }
-
-
-          resolve('done');
-        })
-      }
-      catch (erre) {
-        resolve('done')
-      }
-    },time)
-  })
-}
-
-function doshare() {
-
-  return new Promise(resolve => {
-    setTimeout(() => {
-
-      try {
-        var endurl = '/sign_in/wechat'
-        let body = { "channel": "app" };
-        url = {
-          url: `https://h5.ele.me/restapi/member/v1/users/`,
-          headers: headerscommon,
-          body: JSON.stringify(body)
+        } else {
+            subTitle += '打卡接口请求失败，详情请见日志。'
+            detail += error
+            $cmp.log("elem failed response : \n" + error)
         }
-        if (cookieVal == undefined || cookieVal == "0" || cookieVal == null) {
-          sy.msg(cookieName, "未获取Cookie", '');
-          return;
-        }
-        url.url += userid;
-        url.url += endurl;
-        sy.post(url, (error, response, data) => {
-          if (response.status == 200) {
-
-            sy.log("分享微信成功");
-          }
-          else {
-            sy.log("分享微信失败");
-          }
-
-
-          resolve('done');
-        })
-      }
-      catch (erre) {
-        resolve('done')
-      }
+        $cmp.notify('饿了么 - 打卡领红包', subTitle, detail)
     })
-  })
 }
 
-function dosignhis() {
-
-  return new Promise(resolve => {
-    setTimeout(() => {
-
-      try {
-        var endurl = '/sign_in/info'
-        url = { url: `https://h5.ele.me/restapi/member/v1/users/`, headers: headerscommon }
-        if (cookieVal == undefined || cookieVal == "0" || cookieVal == null) {
-          sy.msg(cookieName, "未获取Cookie", '');
-          return;
+function compatibility() {
+    const isRequest = typeof $request != "undefined"
+    const isSurge = typeof $httpClient != "undefined"
+    const isQuanX = typeof $task != "undefined"
+    const isJSBox = typeof $app != "undefined" && typeof $http != "undefined"
+    const isNode = typeof require == "function" && !isJSBox
+    const node = (() => {
+        if (isNode) {
+            const request = require('request')
+            return ({request})
+        } else {
+            return (null)
         }
-        url.url += userid;
-        url.url += endurl;
-        sy.get(url, (error, response, data) => {
-
-          var obj = JSON.parse(data);
-
-          hisresult = obj;
-
-
-          resolve('done');
+    })()
+    const notify = (title, subtitle, message) => {
+        if (isQuanX) $notify(title, subtitle, message)
+        if (isSurge) $notification.post(title, subtitle, message)
+        if (isNode) log(title+subtitle+message)
+        if (isJSBox) $push.schedule({title: title, body: subtitle?subtitle+"\n"+message:message})
+    }
+    const write = (value, key) => {
+        if (isQuanX) return $prefs.setValueForKey(value, key)
+        if (isSurge) return $persistentStore.write(value, key)
+    }
+    const read = (key) => {
+        if (isQuanX) return $prefs.valueForKey(key)
+        if (isSurge) return $persistentStore.read(key)
+    }
+    const adapterStatus = (response) => {
+        if (response) {
+            if (response.status) {
+                response["statusCode"] = response.status
+            } else if (response.statusCode) {
+                response["status"] = response.statusCode
+            }
+        }
+        return response
+    }
+    const get = (options, callback) => {
+        if (isQuanX) {
+            if (typeof options == "string") options = { url: options }
+            options["method"] = "GET"
+            $task.fetch(options).then(response => {
+                callback(null, adapterStatus(response), response.body)
+            }, reason => callback(reason.error, null, null))
+        }
+        if (isSurge) $httpClient.get(options, (error, response, body) => {
+            callback(error, adapterStatus(response), body)
         })
-      }
-      catch (erre) {
-        resolve('done')
-      }
-    })
-  })
-}
-
-function doNotify() {
-  var ret = signresult+'\n';
-  for (var i = 0; i < sign_result.length; i++) {
-    ret = ret + '***获得：' + sign_result[i].name + '(' + sign_result[i].amount + ')元🧧\n';
-  }
-  ret = ret + turnstr + '\n';
-  for (var i = 0; i < turnresult.length; i++) {
-    if (turnresult[i].status == 1) {
-      ret = ret + '***获得：' + turnresult[i].prizes[0].name + '(' + turnresult[i].prizes[0].amount + ')元🧧\n';
+        if (isNode) {
+            node.request(options, (error, response, body) => {
+                callback(error, adapterStatus(response), body)
+            })
+        }
+        if (isJSBox) {
+            if (typeof options == "string") options = {url: options}
+            options["header"] = options["headers"]
+            options["handler"] = function (resp) {
+                let error = resp.error
+                if (error) error = JSON.stringify(resp.error)
+                let body = resp.data
+                if (typeof body == "object") body = JSON.stringify(resp.data)
+                callback(error, adapterStatus(resp.response), body)
+            }
+            $http.get(options)
+        }
     }
-  }
-  ret = ret + '签到3天得3元红包，7天抽10-200元🧧';
-
-  sy.msg('饿了么', '', ret);
-}
-
-
-
-function init() {
-  isSurge = () => {
-    return undefined === this.$httpClient ? false : true
-  }
-  isQuanX = () => {
-    return undefined === this.$task ? false : true
-  }
-  getdata = (key) => {
-    if (isSurge()) return $persistentStore.read(key)
-    if (isQuanX()) return $prefs.valueForKey(key)
-  }
-  setdata = (key, val) => {
-    if (isSurge()) return $persistentStore.write(key, val)
-    if (isQuanX()) return $prefs.setValueForKey(key, val)
-  }
-  msg = (title, subtitle, body) => {
-    if (isSurge()) $notification.post(title, subtitle, body)
-    if (isQuanX()) $notify(title, subtitle, body)
-  }
-  log = (message) => console.log(message)
-  get = (url, cb) => {
-    if (isSurge()) {
-      $httpClient.get(url, cb)
+    const post = (options, callback) => {
+        if (isQuanX) {
+            if (typeof options == "string") options = { url: options }
+            options["method"] = "POST"
+            $task.fetch(options).then(response => {
+                callback(null, adapterStatus(response), response.body)
+            }, reason => callback(reason.error, null, null))
+        }
+        if (isSurge) {
+            $httpClient.post(options, (error, response, body) => {
+                callback(error, adapterStatus(response), body)
+            })
+        }
+        if (isNode) {
+            node.request.post(options, (error, response, body) => {
+                callback(error, adapterStatus(response), body)
+            })
+        }
+        if (isJSBox) {
+            if (typeof options == "string") options = {url: options}
+            options["header"] = options["headers"]
+            options["handler"] = function (resp) {
+                let error = resp.error
+                if (error) error = JSON.stringify(resp.error)
+                let body = resp.data
+                if (typeof body == "object") body = JSON.stringify(resp.data)
+                callback(error, adapterStatus(resp.response), body)
+            }
+            $http.post(options)
+        }
     }
-    if (isQuanX()) {
-      url.method = 'GET'
-      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    const log = (message) => console.log(message)
+    const done = (value = {}) => {
+        if (isQuanX) isRequest ? $done(value) : null
+        if (isSurge) isRequest ? $done(value) : $done()
     }
-  }
-  post = (options, callback) => {
-    if (isQuanX()) {
-      if (typeof options == "string") options = { url: options }
-      options["method"] = "POST"
-      $task.fetch(options).then(response => {
-        response["status"] = response.statusCode
-        callback(null, response, response.body)
-      }, reason => callback(reason.error, null, null))
-    }
-    if (isSurge()) $httpClient.post(options, callback)
-  }
-  done = (value = {}) => {
-    $done(value)
-  }
-  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
-}
-
-
-
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+    return { isQuanX, isSurge, isJSBox, isRequest, notify, write, read, get, post, log, done }
 }
